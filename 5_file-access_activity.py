f46b10675b32cba8a85c7a324bccec38f6ff6bbf
faustoyg <110213390+faustoyg@users.noreply.github.com>
2022-08-07 10:42:02 -0500

    Delete 5_file-access_activity.py

--- 5_file-access_activity.py
+++
@@ -1,9 +0,0 @@
-
-file = open("devices.txt", mode = 'a')
-while True:
-    newItem = input("Ingrese nuevo device: ")
-    if newItem == "exit":
-        print("All done")
-        exit()
-    file.write(newItem + "\n")
-file.close()
